46b219872313d3cfaf1a60286de3c77eebdd2194
Akira Sawada <akira@peatix.com>
2012-12-23 19:15:08 +0900

    Initial commit for chat and maps

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-
+  ;
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -13,6 +13,9 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', {
+    layout: false
+  });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -20,16 +23,101 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
-
 app.get('/', routes.index);
 
+app.get('/create', function (req, res) {
+  var roomid = app.createRoom();
+  res.redirect('/room/' + roomid);
+});
+
+app.get('/room/:id', function (req, res) {
+  var roomid = req.params.id;
+  if ( roomid.match(/\W/) ) {
+    res.redirect('/');
+    return;
+  }
+  if ( !rooms[roomid] ) {
+    roomid = app.createRoom(roomid);
+  }
+  res.render('room', {
+    title: 'room'
+    , roomid: req.params.id
+    , socket: 'ws://10.0.1.102:3000/' + req.params.id
+  });
+});
+
+// Bootstrap
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+
+
+//socket.io
+var io = require('socket.io').listen(app);
+var rooms = {};
+
+var randSource = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+
+app.createRoom = function (roomid) {
+  var i, count = 0, room, log = [], users = {};
+  if ( !roomid ) {
+    do {
+      roomid = '';
+      for ( i=0;i<6;i++ ) {
+        roomid += randSource[ Math.floor(Math.random() * randSource.length )];
+      }
+    } while ( rooms[roomid] );
+  }
+  room = rooms[roomid] = io
+    .of('/' + roomid)
+    .on('connection', function (socket) {
+      count++;
+      var uid = count;
+      var user = { name: 'user(' + count + ')', id: uid };
+      users[uid] = user;
+      var welcome = {
+        count: count
+        , users: users
+        , you: user
+      };
+      socket.emit('welcome', welcome);
+      socket.broadcast.emit('join', {user:user});
+
+      socket.on('talk', function (msg) {
+        log.push({ user: user, msg: msg });
+        socket.emit('msg push', { user: user, msg: msg});
+        socket.broadcast.emit('msg push', { user: user, msg: msg});
+      });
+      socket.on('rename', function (msg) {
+        user.name = msg;
+      });
+      // geo location
+      socket.on('loc', function(msg) {
+        user.lat = msg.lat;
+        user.lng = msg.lng;
+        socket.emit('loc', { user: user });
+        socket.broadcast.emit('loc', { user: user });
+      });
+
+      socket.on('disconnect', function() {
+        count--;
+        if ( !count ) {
+          console.log('remove ', roomid);
+          rooms[roomid] = null;
+          return;
+        }
+        delete users[uid];
+        socket.broadcast.emit('leave', { user: user });
+      });
+    })
+  ;
+  return roomid;
+};
---
+++ public/javascripts/chat.js
@@ -0,0 +1,154 @@
+$(function() {
+  var latlng = new google.maps.LatLng(35.709984,139.810703);
+  var opts = {
+    zoom: 15,
+    center: latlng,
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var Map = new google.maps.Map(document.getElementById("map"), opts);
+
+  var User = function (opts) {
+    return this.init(opts);
+  };
+  User.prototype = {
+    init: function (opts) {
+      this.$el = $('<li class="user" />');
+      this.$el.appendTo('#users');
+      this.marker = new google.maps.Marker({});
+      this.marker.setMap(Map);
+      this.update(opts);
+    }
+    , update: function (opts) {
+      var shouldUpdateLatLng = false;
+      if ( this.lat !== opts.lat || this.lng !== opts.lng ) {
+        shouldUpdateLatLng = true;
+      }
+      $.extend(this, opts);
+      if ( shouldUpdateLatLng ) this.updateLatLng();
+      this.render();
+    }
+    , updateLatLng: function () {
+      var latlng = new google.maps.LatLng(this.lat, this.lng);
+      this.marker.setPosition( latlng );
+      if ( this.following ) {
+        Map.setCenter(latlng);
+      }
+    }
+    , render: function () {
+      var html = '<a href="#" class="username" data-uid="' + this.id + '">' + this.name + '</a>';
+      this.$el.empty().append( $(html) );
+    }
+    , remove: function () {
+      this.$el.remove();
+      this.marker.setMap(null);
+    }
+    , follow: function () {
+      this.following = true;
+      this.updateLatLng();
+    }
+    , unfollow: function () {
+      this.$el.removeClass('following');
+      this.following = false;
+    }
+  };
+
+  var UserCollection = {
+    users: {}
+    , init: function (users) {
+      for ( uid in users ) {
+        this.users[uid] = new User( users[uid] );
+      }
+    }
+    , add: function (user) {
+      this.users[user.id] = new User( user );
+    }
+    , update: function (opts) {
+      if (typeof this.users[opts.id] === 'undefined' ) {
+        this.add(opts);
+      }
+      this.users[opts.id].update(opts);
+    }
+    , remove: function (opts) {
+      this.users[opts.id].remove(opts);
+      delete this.users[opts.id];
+    }
+    , follow: function (opts) {
+      for ( uid in this.users ) {
+        this.users[uid].unfollow();
+      }
+      this.users[opts.id].follow();
+    }
+  };
+
+  var ChatClient = function (opts) {
+    return this.init(opts);
+  };
+
+  ChatClient.prototype = {
+    init: function (opts) {
+      this.users = UserCollection;
+      var chat = this
+        , socket = this.socket = io.connect(opts.socket)
+        ;
+      socket.on('connect', function() {
+        console.log('connected');
+      });
+
+      socket.on('msg push', function (msg) {
+        chat.log( msg.user.name + ': ' + msg.msg );
+      });
+
+      socket.on('welcome', function (msg) {
+        chat.users.init(msg.users);
+//        chat.users.add msg.you;
+
+      });
+
+      socket.on('join', function (msg) {
+        chat.users.add(msg.user);
+        chat.log(msg.user.name + ' joined');
+      });
+
+      socket.on('leave', function (msg) {
+        chat.log(msg.user.name + ' leaved');
+        chat.users.remove(msg.user);
+      });
+
+      socket.on('loc', function (msg) {
+        chat.users.update(msg.user);
+      });
+
+      $('#posttext').click(function() {
+        var msg = $('#text').val();
+        socket.emit('talk', msg);
+      });
+
+      $('#rename').click(function () {
+        var name = $('#name').val();
+        socket.emit('rename', name);
+      });
+
+      $('.username').live('click',function () {
+        chat.users.follow({ id: $(this).attr('data-uid') });
+        return false;
+      });
+
+      $('#sendloc').click( function () {
+        navigator.geolocation.getCurrentPosition( function (e) {
+          chat.socket.emit('loc', {lat: e.coords.latitude, lng: e.coords.longitude });
+        });
+      });
+
+      navigator.geolocation.watchPosition(function (e) {
+        chat.socket.emit('loc', {lat: e.coords.latitude, lng: e.coords.longitude });
+      });
+    }
+    , log: function (msg) {
+      $('#log').prepend($('<li>' + msg + '</li>'));
+    }
+  };
+
+
+  window.ChatClient = ChatClient;
+});
